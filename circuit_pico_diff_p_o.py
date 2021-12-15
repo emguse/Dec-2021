9bf6463c7b631f1c2aa6d1313370668532e0205a
emguse <30695541+emguse@users.noreply.github.com>
2021-12-15 19:38:00 +0900

    Change part of Main to a class function

--- circuit_pico_diff_p_o.py
+++ circuit_pico_diff_p_o.py
@@ -26,12 +26,20 @@ PRINTER_TX = board.GP12
 BUTTON_A = board.GP16
 BUTTON_B = board.GP17
 TIME_ADJUSTING = False
+TIME_TO_SET = (2021, 12, 13, 20, 45, 00, 0, -1, -1)
+# TIME_TO_SET = (year, mon, date, hour, min, sec, wday, yday, isdst)
+# Year, month, day, hour, minute, second, and weekday are required.
+# weekday is Number between [0,6], where Monday is 0
+# Substitute "-1" for "yearday, isdst".
 
 class DifferentialPressureLogger():
-    def __init__(self, i2c: I2C) -> None:
+    def __init__(self) -> None:
+        i2c = busio.I2C(scl=I2C_SCL, sda=I2C_SDA)
         self.rb_p = RingBuffer(QUE_SIZE)
         self.rb_ref = RingBuffer(REFARENCE_PAST_SAMPLE)
         self.d6f_ph0505 = D6F_PH0505(i2c)
+        self.rtc = RtcDs3231(i2c)
+        self.thermal_printer = PrinterDpEh600()
         self.ma_p = 0
         self.past_sample = 0
         self.button_up = digitalio.DigitalInOut(BUTTON_A)
@@ -39,6 +47,7 @@ class DifferentialPressureLogger():
         self.button_down = digitalio.DigitalInOut(BUTTON_B)
         self.button_down.switch_to_input(pull = digitalio.Pull.DOWN) 
         self.threshold = THRESHOLD
+        self.buzzer = PiPi()
     def read_dp(self) -> Float:
         self.d6f_ph0505.start_order()
         self.d6f_ph0505.read_order()
@@ -47,6 +56,27 @@ class DifferentialPressureLogger():
         self.ma_p = self.read_dp() - ZERO_OFFSET
         self.rb_p.append(self.ma_p)
         self.rb_ref.append(self.ma_p)
+    def time_adjusting(self) -> None:
+        self.rtc.time_adjusting = True
+        self.rtc.time_to_set = TIME_TO_SET
+        self.rtc.adjust()
+        t = self.rtc.read()
+        print(t)
+        self.rtc.time_adjusting = False
+    def timestamp(self) -> None:
+        t = self.rtc.read()
+        print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+        self.thermal_printer.printer.print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+    def threshold_up(self) -> None:
+        print("up")
+        self.threshold = round(self.threshold + 0.1, 3)
+        self.thermal_printer.printer.print("THRESHOLD:"+ str(self.threshold))
+        time.sleep(0.5)
+    def threshold_down(self) -> None:
+        print("down")
+        self.threshold = round(self.threshold - 0.1, 3)
+        self.thermal_printer.printer.print("THRESHOLD:"+ str(self.threshold))
+        time.sleep(0.5)
 
 class PiPi():
     def __init__(self) -> None:
@@ -70,26 +100,20 @@ class PrinterDpEh600:
         self.printer.feed(lines)
 
 def main():
-    i2c = busio.I2C(scl=I2C_SCL, sda=I2C_SDA)
-    logger = DifferentialPressureLogger(i2c)
-    ma = MovingAverage(MOVE_AVE_LENGTH, True)
-    buzzer = PiPi()
-    thermal_printer = PrinterDpEh600()
-    rtc = RtcDs3231(i2c)
-    if TIME_ADJUSTING == True:
-        rtc.time_adjusting = True
-        rtc.time_to_set = (2021, 12, 14, 17, 36, 00, 1, -1, -1)
-        rtc.adjust()
-        t = rtc.read()
-        print(t)
+    logger = DifferentialPressureLogger()
+    ma = MovingAverage(MOVE_AVE_LENGTH, True)    
     past_time = 0
-    t = rtc.read()
-    print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-    thermal_printer.printer.print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-    thermal_printer.printer.print("THRESHOLD:"+ str(logger.threshold))
+
+    if TIME_ADJUSTING == True:   
+        logger.time_adjusting()
+    
+    logger.timestamp()
+    logger.thermal_printer.printer.print("THRESHOLD:"+ str(logger.threshold))
+
     for _ in range(MOVE_AVE_LENGTH):
         logger.read_and_record()
         logger.past_sample = logger.ma_p
+
     while True:
         logger.read_and_record()
         delta = logger.past_sample - logger.ma_p
@@ -97,11 +121,10 @@ def main():
             if abs(delta) >= logger.threshold:
                 #print("diff_p:" + str(round(ma_p, 4)) + "  Δ:" + str(round(delta, 4)) + "  time:" + str(time.time()))
                 print((round(logger.ma_p, 4), round(delta, 4)))
-                t = rtc.read()
-                thermal_printer.printer.print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-                thermal_printer.printer.print(str("diff_P:" + str(round(logger.ma_p, 4)) + ", delta:" + str(round(delta, 4))))
-                thermal_printer.printer.feed(1)
-                buzzer.pi()
+                logger.buzzer.pi()
+                logger.timestamp()
+                logger.thermal_printer.printer.print(str("diff_P:" + str(round(logger.ma_p, 4)) + ", delta:" + str(round(delta, 4))))
+                logger.thermal_printer.printer.feed(1)
                 past_time = time.time() + IVENT_LENGTH
                 for _ in range(QUE_SIZE):
                     logger.ma_p = logger.read_dp() - ZERO_OFFSET
@@ -113,15 +136,9 @@ def main():
                 Forward_p.extend(after_p)
         past_sample = logger.ma_p
         if logger.button_up.value:
-            print("up")
-            logger.threshold = round(logger.threshold + 0.1, 3)
-            thermal_printer.printer.print("THRESHOLD:"+ str(logger.threshold))
-            time.sleep(0.5)
+            logger.threshold_up()
         if logger.button_down.value:
-            print("down")
-            logger.threshold = round(logger.threshold - 0.1, 3)
-            thermal_printer.printer.print("THRESHOLD:"+ str(logger.threshold))
-            time.sleep(0.5)
+            logger.threshold_down()
 
 if __name__ == "__main__":
     main()
