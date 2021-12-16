86c784821c1659be3c6e9b292e12fb198e5cbb2b
emguse <30695541+emguse@users.noreply.github.com>
2021-12-16 21:03:37 +0900

    Prepare for SD card connection

--- circuit_pico_diff_p_o.py
+++ circuit_pico_diff_p_o.py
@@ -7,14 +7,15 @@ import time
 import busio
 import adafruit_thermal_printer
 from circuit_rtc_ds3231 import RtcDs3231
+import sdcardio
 
 CYCLE_TIME = 0.033  # sec
 IVENT_LENGTH = 10  # sec
-QUE_SIZE = int(IVENT_LENGTH/2 * 1/CYCLE_TIME)
+QUE_SIZE = int(IVENT_LENGTH / 2 * 1 / CYCLE_TIME)
 MOVE_AVE_LENGTH = 2
 REFARENCE_PAST_SAMPLE = 2
 THRESHOLD = 0.5
-ZERO_OFFSET = 0 # Zero point correction
+ZERO_OFFSET = 0  # Zero point correction
 USE_PRINTER = False
 EXPORT_CSV = False
 EXPORT_WAV = False
@@ -25,6 +26,10 @@ PRINTER_RX = board.GP13
 PRINTER_TX = board.GP12
 BUTTON_A = board.GP16
 BUTTON_B = board.GP17
+SPI_SCK = board.GP18
+SPI_MOSI = board.GP19
+SPI_MISO = board.GP20
+SPI_CS = board.GP21
 TIME_ADJUSTING = False
 TIME_TO_SET = (2021, 12, 13, 20, 45, 00, 0, -1, -1)
 # TIME_TO_SET = (year, mon, date, hour, min, sec, wday, yday, isdst)
@@ -32,7 +37,7 @@ TIME_TO_SET = (2021, 12, 13, 20, 45, 00, 0, -1, -1)
 # weekday is Number between [0,6], where Monday is 0
 # Substitute "-1" for "yearday, isdst".
 
-class DifferentialPressureLogger():
+class DifferentialPressureLogger:
     def __init__(self) -> None:
         i2c = busio.I2C(scl=I2C_SCL, sda=I2C_SDA)
         self.rb_p = RingBuffer(QUE_SIZE)
@@ -43,9 +48,9 @@ class DifferentialPressureLogger():
         self.ma_p = 0
         self.past_sample = 0
         self.button_up = digitalio.DigitalInOut(BUTTON_A)
-        self.button_up.switch_to_input(pull = digitalio.Pull.DOWN)
+        self.button_up.switch_to_input(pull=digitalio.Pull.DOWN)
         self.button_down = digitalio.DigitalInOut(BUTTON_B)
-        self.button_down.switch_to_input(pull = digitalio.Pull.DOWN) 
+        self.button_down.switch_to_input(pull=digitalio.Pull.DOWN)
         self.threshold = THRESHOLD
         self.buzzer = PiPi()
     def read_dp(self) -> Float:
@@ -65,23 +70,32 @@ class DifferentialPressureLogger():
         self.rtc.time_adjusting = False
     def timestamp(self) -> None:
         t = self.rtc.read()
-        print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
-        self.thermal_printer.printer.print("{}-{}-{}T{}:{}:{}".format(t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec))
+        print(
+            "{}-{}-{}T{}:{}:{}".format(
+                t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec
+            )
+        )
+        self.thermal_printer.printer.print(
+            "{}-{}-{}T{}:{}:{}".format(
+                t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec
+            )
+        )
     def threshold_up(self) -> None:
         print("up")
         self.threshold = round(self.threshold + 0.1, 3)
-        self.thermal_printer.printer.print("THRESHOLD:"+ str(self.threshold))
+        self.thermal_printer.printer.print("THRESHOLD:" + str(self.threshold))
         time.sleep(0.5)
     def threshold_down(self) -> None:
         print("down")
         self.threshold = round(self.threshold - 0.1, 3)
-        self.thermal_printer.printer.print("THRESHOLD:"+ str(self.threshold))
+        self.thermal_printer.printer.print("THRESHOLD:" + str(self.threshold))
         time.sleep(0.5)
 
-class PiPi():
+class PiPi:
     def __init__(self) -> None:
         self.buzzer = digitalio.DigitalInOut(board.GP6)
         self.buzzer.direction = digitalio.Direction.OUTPUT
+
     def pi(self) -> None:
         self.buzzer.value = True
         time.sleep(0.03)
@@ -101,14 +115,17 @@ class PrinterDpEh600:
 
 def main():
     logger = DifferentialPressureLogger()
-    ma = MovingAverage(MOVE_AVE_LENGTH, True)    
+    ma = MovingAverage(MOVE_AVE_LENGTH, True)
     past_time = 0
-
-    if TIME_ADJUSTING == True:   
-        logger.time_adjusting()
     
+    spi = busio.SPI(SPI_SCK, MOSI=SPI_MOSI, MISO=SPI_MISO)
+    cs = SPI_CS
+    #sdcard = sdcardio.SDCard(spi, cs)
+
+    if TIME_ADJUSTING == True:
+        logger.time_adjusting()
     logger.timestamp()
-    logger.thermal_printer.printer.print("THRESHOLD:"+ str(logger.threshold))
+    logger.thermal_printer.printer.print("THRESHOLD:" + str(logger.threshold))
 
     for _ in range(MOVE_AVE_LENGTH):
         logger.read_and_record()
@@ -119,12 +136,19 @@ def main():
         delta = logger.past_sample - logger.ma_p
         if past_time <= time.time():
             if abs(delta) >= logger.threshold:
-                #print("diff_p:" + str(round(ma_p, 4)) + "  Δ:" + str(round(delta, 4)) + "  time:" + str(time.time()))
+                # print("diff_p:" + str(round(ma_p, 4)) + "  Δ:" + str(round(delta, 4)) + "  time:" + str(time.time()))
                 print((round(logger.ma_p, 4), round(delta, 4)))
-                logger.buzzer.pi()
                 logger.timestamp()
-                logger.thermal_printer.printer.print(str("diff_P:" + str(round(logger.ma_p, 4)) + ", delta:" + str(round(delta, 4))))
+                logger.thermal_printer.printer.print(
+                    str(
+                        "diff_P:"
+                        + str(round(logger.ma_p, 4))
+                        + ", delta:"
+                        + str(round(delta, 4))
+                    )
+                )
                 logger.thermal_printer.printer.feed(1)
+                logger.buzzer.pi()
                 past_time = time.time() + IVENT_LENGTH
                 for _ in range(QUE_SIZE):
                     logger.ma_p = logger.read_dp() - ZERO_OFFSET
