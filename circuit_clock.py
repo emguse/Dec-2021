081fd0429c366f3c6c41a57a7035fef037e985a2
emguse <30695541+emguse@users.noreply.github.com>
2021-12-24 22:29:39 +0900

    Add Digital IO and a class to sound a buzzer at 0 and 30 minutes every hour

--- circuit_clock.py
+++ circuit_clock.py
@@ -2,20 +2,22 @@ import time
 import board
 import busio
 import neopixel
+import digitalio
 from rainbowio import colorwheel
 from circuit_rtc_ds3231 import RtcDs3231
 from adafruit_ht16k33.segments import BigSeg7x4
 
 '''
-- 2021/12/22 ver.0.10
+- 2021/12/22 ver.0.01
 - Author : emguse
 - License: MIT License
 Hardware requirements
 - Adafruit QT Py RP2040
-- Adafruit DS3231 Precision RTC Breakout
+- Adafruit DS3231 Precision RTC-STEMMA QT
 - Adafruit 1.2" 4-Digit 7-Segment Display w/I2C Backpack - Yellow
 '''
 
+BUZZER_PIN = board.D4
 TIME_ADJUSTING = False
 TIME_TO_SET = (2021, 12, 23, 20, 21, 00, 4, -1, -1)
 # TIME_TO_SET = (year, mon, date, hour, min, sec, wday, yday, isdst)
@@ -44,14 +46,23 @@ class OnbordNeopix():
         self.pixel[0] = colorwheel(self.color_step & 255)
         self.pixel.show()
 
+class PiPi:
+    def __init__(self) -> None:
+        self.buzzer = digitalio.DigitalInOut(BUZZER_PIN)
+        self.buzzer.direction = digitalio.Direction.OUTPUT
+    def pi(self, count) -> None:
+        for i in range(count):
+            self.buzzer.value = True
+            time.sleep(0.03)
+            self.buzzer.value = False
+
 def main():
     i2c = busio.I2C(board.SCL1, board.SDA1)
     rtc = RtcDs3231(i2c)
     display = BigSeg7x4(i2c)
     display.brightness = 0.5
-    #display.blink_rate = 3
-
     onbord_neopix = OnbordNeopix()
+    buzzer = PiPi()
 
     if TIME_ADJUSTING:
         rtc.time_adjusting = True
@@ -70,10 +81,16 @@ def main():
             ))
         print(tstamp)
         display.print("{:02}{:02}".format(t.tm_hour, t.tm_min))
+        # Blinking cologne
         if t.tm_sec % 2 == 0:
             display.colon = True
         else:
             display.colon = False
+        if t.tm_min + t.tm_sec == 0:
+            buzzer.pi(2)
+        if t.tm_min == 30:
+            if t.tm_sec == 0:
+                buzzer.pi(1)
         time.sleep(1)
 
 if __name__ == '__main__':
